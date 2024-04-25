e0d5110b96d053e119d754af13bb0df5b202a384
Jegou Sam <sam.jegou@student-cs.fr>
2024-04-25 18:47:16 +0200

    Comparison Pipeline & Correction Ridge

--- main_Sam.py
+++ main_Sam.py
@@ -7,23 +7,49 @@ from utils_Sam import *
 ### Global variables
 np.random.seed(42)
 
-
-n,d = 10,2
+n,d = 100,20
+intern = 5
 lambda_ = 1.
 
+snr = 10 # signal to noise ratio
 
-A = np.random.rand(n,d)
-Ws = [np.random.rand(d,d) for _ in range(2)] + [A]
-b = np.random.rand(n)
+### Define the problem
+input = np.random.rand(n,d)
+noise = np.random.rand(n,d)
+output = np.random.rand(n)*snr
+output_noise = output + np.random.rand(n)
 
-x = ridge(A,b,lambda_)
-obj = objective(A,b,x)
+### Ridge solution
+sol_ridge = ridge(input,output,lambda_)
+obj = objective(input,output,sol_ridge)
 
-x_nonlin = solve_nonlinear_ridge(Ws,b,lambda_)
-obj_nonlin = objective_nonlinear(Ws,b,x_nonlin)
+sol_ridge_noise = ridge(input,output_noise,lambda_)
+obj_noise = objective(input,output_noise,sol_ridge_noise)
 
-print(f'Solution: {x}')
-print(f'Objective: {obj:.3e}')
+### Define MLP
+MLP = Multi_Layer_Perceptron(input_dim=d,
+                             intern_dim=intern,
+                             output_dim=n,
+                             depth = 1)
 
-print(f'Solution non-linear: {x_nonlin}')
-print(f'Objective non-linear: {obj_nonlin:.3e}')
+input = (torch.from_numpy(input).to(torch.float32))
+output = (torch.from_numpy(output).to(torch.float32))
+output_noise = (torch.from_numpy(output_noise).to(torch.float32))
+
+#train(MLP, input, output, init_norm = None, epochs = 40000, debug = 1)
+#train(MLP, input, output, init_norm = 0, epochs = 40000, debug = 1)
+#train(MLP, input, output, init_norm = 1, epochs = 40000, debug = 1)
+train(MLP, input, output, init_norm = 2, epochs = 1000, debug = 1, savename='no_noise_Sam.pt')
+sol_MLP = torch.eye(d)
+for layer in MLP.children():
+    sol_MLP = layer.weight@sol_MLP
+
+compare(input, output, sol_ridge, sol_MLP)
+#train(MLP, input, output, init_norm = 10, epochs = 40000, debug = 1)
+
+train(MLP, input, output_noise, init_norm = 2, epochs = 1000, debug = 1, savename='noise_Sam.pt')
+sol_MLP_noise = torch.eye(d)
+for layer in MLP.children():
+    sol_MLP_noise = layer.weight@sol_MLP
+
+compare(input, output_noise, sol_ridge_noise, sol_MLP_noise)
--- utils_Sam.py
+++ utils_Sam.py
@@ -1,35 +1,78 @@
 import numpy as np
 import numbers
+from collections import OrderedDict
+import torch
+import torch.nn as nn
+import math
+
+DIRPATH = 'models/'
 
 ### Define objective function
 def objective(A,b,x):
-    return np.sum((A@x-b)**2)/(2*A.shape[0])
+    return ((A@x-b)**2).sum()/(2*A.shape[0])
 
 def objective_nonlinear(Ws,b,x):
     A = Ws[0]
     for k in range(1,len(Ws)):
         A = Ws[k]@A
-    return np.sum((A@x-b)**2)/(2*A.shape[0])
+    return ((A@x-b)**2).sum()/(2*A.shape[0])
 
 ### Ridge regression (L2 penalization)
 def ridge(A,b,lambda_):
     '''
-    Returns the solution of min 1/(2*n_samples)*||Ax-b||^2+||lambda_*x||^2 where
-    lambda_ is 0D, 1D (A.shape[0]=n_samples,) or 2D (A.shape)
+    Solves min 1/(2*n)*||Ax-b||^2+||lambda_*x||^2
+
+    Parameters
+    ----------
+    A: (n,d) array
+        Design matrix
+    
+    b: (n,) array
+        Observations
+    
+    lambda_: number, (n,) or (n,d) array
+        L2 penalization parameter
+    
+    Returns
+    -------
+    x: (d,) array
+        The solution of the ridge regression
     '''
+    n,d = A.shape
     if isinstance(lambda_, numbers.Number): # real number
-        inv = np.linalg.inv(A.T@A+lambda_**2*np.eye(A.shape[1]))
+        if d<=n: # underparametrized
+            inv = np.linalg.inv(1/n*A.T@A+lambda_**2*np.eye(d))
+            res = inv@(A.T)@b
+        else: # overparametrized
+            inv = np.linalg.inv(1/n*A@(A.T)+lambda_**2*np.eye(n))
+            res = A.T@inv@b
     else: # multi-dim
         if lambda_.ndim == 1: # assumes regularization per dimension
-            inv = np.linalg.inv(A.T@A+np.eye(lambda_**2))
+            inv = np.linalg.inv(1/n*A.T@A+np.eye(lambda_**2))
         elif lambda_.ndim == 2: # assumes matrix regularization
-            inv = np.linalg.inv(A.T@A+lambda_.T@lambda_)
-    res = 1/A.shape[0]*inv@(A.T)@b
+            inv = np.linalg.inv(1/n*A.T@A+lambda_.T@lambda_)
+        res = inv@(A.T)@b
     return res
 
 def solve_nonlinear_ridge(Ws, b, lambda_):
     '''
     Solves min ||Ws[-1]@...@W[0]*x - b||^2 + ||lambda_*x||^2
+
+    Parameters
+    ----------
+    Ws: list of (n,d) arrays
+        Weight matrices
+    
+    b: (n,) array
+        Observations
+    
+    lambda_: number, (n,) or (n,d) array
+        L2 penalization parameter
+    
+    Returns
+    -------
+    x: (d,) array
+        The solution of the non-linear ridge regression (see above)
     '''
     A = Ws[0]
     for k in range(1,len(Ws)):
@@ -37,3 +80,67 @@ def solve_nonlinear_ridge(Ws, b, lambda_):
     x = ridge(A,b,lambda_)
     return x
 
+### MLP
+
+class Multi_Layer_Perceptron(nn.Sequential):
+    def __init__(self, input_dim, intern_dim, output_dim, depth = 2, isBiased = False):
+        
+        dict = OrderedDict([("input",nn.Linear(input_dim,intern_dim, bias=isBiased))])
+        for i in range(depth):
+            dict.update({str(i) : nn.Linear(intern_dim,intern_dim,bias=isBiased)})
+        dict.update({"output" : nn.Linear(intern_dim,output_dim,bias=isBiased)})
+
+        super().__init__(dict)
+        
+        self.reset_init_weights_biases() # so that we do not use a default initialization
+
+    def reset_init_weights_biases(self, norm = None):
+        for layer in self.children():
+            if norm == None:
+                stdv = 1. / math.sqrt(layer.weight.size(1))
+            else :
+                stdv = norm
+            
+            layer.weight.data.uniform_(-stdv, stdv)
+            if layer.bias is not None:
+                layer.biases.data.uniform_(-stdv, stdv)
+
+def train(model, input_data, output_data, lossFct = nn.MSELoss(), optimizer = None, epochs = 20, init_norm = None, save = True, debug = False, savename='model.pt'):
+
+    if optimizer is None:
+        optimizer = torch.optim.SGD(model.parameters())
+    
+    if init_norm is not None:
+        model.reset_init_weights_biases(init_norm)
+
+    for i in range(epochs):
+        y_pred = model(input_data)
+        loss = lossFct(y_pred, output_data)
+
+        if math.isnan(loss.item()):
+            print(f"Epoch: {i+1}   Loss: {loss.item()}")
+            break
+            
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if debug:
+            if (i+1)%(epochs/debug) == 0:
+                print(f"Epoch: {i+1}   Loss: {loss.item()}")
+        
+        if save:
+            torch.save(model.state_dict(), DIRPATH+savename)
+    
+### Comparison of models
+def compare(input, output, w1, w2):
+    res1 = objective(input, output, w1)
+    res2 = objective(input, output, w2)
+
+    print('Model 1:')
+    print(f'   - objective: {res1:.3e}')
+    print(f'   - weights norm: {np.linalg.norm(w1):.2f}')
+
+    print('Model 2:')
+    print(f'   - objective: {res2:.3e}')
+    print(f'   - weights norm: {np.linalg.norm(w2):.2f}')
